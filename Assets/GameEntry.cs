95fe28c5455615b09ff16498a88cd4dc5987a6f8
marcuslelus <44736661+marcuslelus@users.noreply.github.com>
2019-11-20 15:17:06 -0500

    Initial commit

---
+++ Assets/GameEntry.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEntry : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameFlow.Instance.FirstInitialization();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameFlow.Instance.SecondInitialization();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameFlow.Instance.Refresh();
+    }
+    private void FixedUpdate()
+    {
+        GameFlow.Instance.PhysicsRefresh();
+    }
+}
---
+++ Assets/GameFlow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlow : IManager
+{
+    #region singleton
+    private static GameFlow instance;
+    private GameFlow() { }
+    public static GameFlow Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameFlow();
+            }
+            return instance;
+        }
+    }
+    #endregion
+    public void FirstInitialization()
+    {
+    }
+    //like start func
+    public void SecondInitialization()
+    {
+    }
+    public void Refresh()
+    {
+    }
+    public void PhysicsRefresh()
+    {
+    }
+}
