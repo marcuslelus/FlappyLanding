769a07b8bb2585f3a6c3eec04a2ccd220899cbd5
marcuslelus <44736661+marcuslelus@users.noreply.github.com>
2019-11-21 11:06:34 -0500

    first commit

---
+++ Assets/Scenes/Scripts/WallManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : IManager
+{
+    #region singleton
+    private static WallManager instance;
+    private WallManager() { }
+    public static WallManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new WallManager();
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    List<Wall> walls;
+
+    public void FirstInitialization()
+    {
+        walls = new List<Wall>();
+        foreach(Transform wall in GameObject.FindGameObjectWithTag("Walls").transform)
+        {
+            walls.Add(wall.GetComponent<Wall>());
+        }
+    }
+
+    public void PhysicsRefresh()
+    {
+        foreach (Wall wall in walls)
+        {
+            wall.PhysicsRefresh();
+        }
+    }
+
+    public void Refresh()
+    {
+
+    }
+
+    public void SecondInitialization()
+    {
+        throw new System.NotImplementedException();
+    }
+}
